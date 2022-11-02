3799f71be054c5316288b9cc6d23e4837df6945d
David <isaacsdavid28@gmail.com>
2022-11-02 18:33:37 -0400

    first commit

---
+++ Lab7-ReadySetActionDOM/readySet.js
@@ -0,0 +1,32 @@
+function main(){
+
+
+
+let growMe = document.querySelector("#grow-me");
+
+growMe.classList.add("big")
+
+let shrinkMe = document.querySelector("#shrink-me");
+
+shrinkMe.classList.remove("big")
+
+let list = document.querySelectorAll("li")
+
+
+// need list items written out
+
+
+document.getElementsByClassName("link").innerHTML = "somewhere";
+document.getElementsByClassName("link").setAttribute ("href","https://www.example.com");
+// need nowhere to be replace by somwhere
+
+// for(let tag of linkChange){
+//     tag.innerHTML `<a href="https://www.example.com">somewhere`
+//     //tag.setAttribute("href","https://www.example.com")
+// }
+
+let nameChange
+
+
+
+}
