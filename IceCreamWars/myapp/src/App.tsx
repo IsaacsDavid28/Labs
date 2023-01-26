f0dc051964de434a3c8f7c6193aa6320110714df
David <isaacsdavid28@gmail.com>
2023-01-26 18:31:02 -0500

    ice cream lab

---
+++ IceCreamWars/myapp/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header'
+import {AdDesign} from './components/AdDesigner'
+import { Votes } from './components/Votes';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div  className="main">
+      <AdDesign />
+      <Votes/>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ IceCreamWars/myapp/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import '../css/Header.css'
+
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="Header">
+            <header><h2>Ice Cream Wars</h2></header>
+            
+        </div>
+    )
+}
+
+export default Header;
